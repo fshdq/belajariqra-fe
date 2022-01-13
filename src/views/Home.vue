7bc127950cc6aecd568779fdfc66aeb8b2d319cb
fshdq <m.farras@rocketmail.com>
2022-01-13 18:43:05 +0700

    add illustration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="bg-blue-100">
-    <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-      
+  <div class="hero">
+    <div class="max-w-7xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8">
+      <div class="flex flex-col-reverse lg:flex-row lg:items-center justify-center mb-5 md:mb-10 ">
+        <div class="w-full lg:w-1/2 pb-10 lg:order-1 lg:ps-5">
+          <img src="@/assets/hero1.svg" alt="logo" class="w-auto mb-5">
+        </div>
+        <div class="flex-1 w-full max-w-lg mx-auto lg:mx-0 lg:w-1/2 pb-10">
+          <div class="text-center lg:text-left tracking-wide leading-loose">
+              <h2 class="mb-5 text-3xl sm:text-4xl font-black">Belajar Kimia Organik</h2>
+              <p>Rasakan kemudahan belajar Kimia Organik menggunakan <b>Metode Iqra</b> yang cepat, mudah dan singkat.</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<style>
+.hero {
+  display: block;
+  box-sizing: border-box;
+  height: 568px;
+  @apply bg-blue-100;
+  clip-path: ellipse(99% 100% at 50% 0%);
+}
+</style>
