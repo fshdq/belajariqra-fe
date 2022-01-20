5f1f14b52b9e76887f569d0f20939a4f842b89d4
Farras Shiddiq <farrascitizen@gmail.com>
2022-01-20 14:28:07 +0700

    add firebaseconfig and firebasehelper

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,7 +1,4 @@
-import { initializeApp } from 'firebase/app';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const config = {
     apiKey: "AIzaSyB49DDqrAaMdIXveBp4UrCmHneQjvT9PBg",
     authDomain: "belajariqra-54181.firebaseapp.com",
     projectId: "belajariqra-54181",
@@ -9,7 +6,3 @@ const firebaseConfig = {
     messagingSenderId: "817496455725",
     appId: "1:817496455725:web:9c7c77ce30e17b5b5416a4"
   };
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
-
-export default firebaseApp.firestore();
--- src/views/Kursus.vue
+++ src/views/Kursus.vue
@@ -1,3 +1,36 @@
 <template>
-    
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+    <div class="max-w-3xl mx-auto">
+      <div>
+        <div class="flow-root mt-6">
+          <ul role="list" class="-my-5 divide-y divide-gray-200">
+              <li v-for="data in kursuses" :key="data.id" class="py-5">
+              <div class="relative focus-within:ring-2 focus-within:ring-indigo-500">
+                  <h3 class="text-sm font-semibold text-gray-800">
+                  <a href="#" class="hover:underline focus:outline-none">
+                      <!-- Extend touch target to entire panel -->
+                      <span class="absolute inset-0" aria-hidden="true" />
+                      {{ data.nama_paket }}
+                  </a>
+                  </h3>
+                  <p class="mt-1 text-sm text-gray-600 line-clamp-2">
+                  {{ data.harga }}
+                  </p>
+              </div>
+              </li>
+          </ul>
+        </div>
+        <div class="mt-6">
+          <a href="#" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+              View all
+          </a>
+        </div>
+    </div>
+    </div>
+  </div>
 </template>
+
+<script>
+
+</script>
