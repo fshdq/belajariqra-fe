1914dd948d473b5994ff56aa22699b5ff8607723
Farras Shiddiq <farrascitizen@gmail.com>
2022-03-02 16:40:17 +0700

    add firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAExKiyWbaBrhVuvHvL-Cvt-p17NobyfuA",
+  authDomain: "belajarkimia-iqra.firebaseapp.com",
+  projectId: "belajarkimia-iqra",
+  storageBucket: "belajarkimia-iqra.appspot.com",
+  messagingSenderId: "994122298565",
+  appId: "1:994122298565:web:a7eb0c458b4bdc77e22cac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
